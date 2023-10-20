c94a77b3f4902116e66e3b6d997bee1e50f704e6
Muhammad Putra Wibisono <mputrawibisone993@gmail.com>
2023-10-20 16:28:40 +0800

    fix: dropdown

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -70,63 +70,12 @@ function Header() {
   }, []);
 
   return (
-    // <header className="p-4 fixed w-full z-50">
-    //   <nav className="container flex flex-col justify-between items-center md:flex-row">
-    //     <a
-    //       href="/"
-    //       className="navbar-logo text-4xl font-bold text-red-600 mb-2"
-    //     >
-    //       Movielist
-    //     </a>
-    //     <div className="w-full relative mb-3 md:w-1/2 md:mb-0">
-    //       {user && (
-    //         <>
-    //           <form action="search" onSubmit={handleSearch}>
-    //             <input
-    //               type="text"
-    //               name="search"
-    //               placeholder="What do you want to watch?"
-    //               className="outline-none font-semibold text-md bg-transparent border-none ring-2 ring-red-600 rounded-full border-red-600 w-[100%] px-4 py-1 md:py-2"
-    //             />
-    //             <div className="absolute top-0 right-0 transform translate-y-1 md:translate-y-2  -translate-x-3">
-    //               <img
-    //                 width="25px"
-    //                 height="25px"
-    //                 src="/search.svg"
-    //                 alt="Search.svg"
-    //               />
-    //             </div>
-    //           </form>
-    //         </>
-    //       )}
-    //     </div>
-    //     <div className="flex justify-center items-center gap-2">
-    //       {user ? (
-    //         <>
-    //           <a
-    //             href="/myprofile"
-    //             className="px-3 py-1 md:px-6 md:py-2 bg-transparent hover:decoration-red-600 text-red-600 rounded-full font-bold hover:text-white transition-all duration-300"
-    //           >
-    //             {user?.name}
-    //           </a>
-    //           <button
-    //             type="button"
-    //             className="px-3 py-1 md:px-6 md:py-2 bg-red-600 hover:bg-red-700 border-2 border-red-600 text-white rounded-full font-bold transition-all duration-300"
-    //             onClick={logout}
-    //           >
-    //             Logout
-    //           </button>
-    //         </>
-    //       ) : (
-    //         <></>
-    //       )}
-    //     </div>
-    //   </nav>
-    // </header>
-
     <header className="p-4 fixed w-full z-50">
       <nav className="container flex flex-col justify-between items-center md:flex-row">
-        <a href="/" className="text-4xl font-bold text-red-600 mb-2 navbar">
+        <a
+          href="/"
+          className="text-4xl font-bold text-red-600 mb-2 navbar-logo"
+        >
           Movielist
         </a>
         <div className="w-full relative mb-3 md:w-1/2 md:mb-0">
@@ -162,7 +111,7 @@ function Header() {
                   <img src="/drop-user2.png" width={30} />
                 </button>
                 {isDropdownOpen && (
-                  <div className="absolute mt-2 bg-white border rounded shadow-md">
+                  <div className="absolute right-0 mt-2 bg-white border rounded shadow-md">
                     <div className="px-4 inline-block py-2 border-b-2 border-slate-900 text-md">
                       Hi! {user.name}
                     </div>
